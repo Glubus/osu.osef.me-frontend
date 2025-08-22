0725367533d889ac17e3418ba565571620fb9737
Osef <Glubus>
2025-08-22 09:18:50 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BeatmapBrowser from './pages/BeatmapBrowser';
 import BeatmapDetail from './pages/BeatmapDetail';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    document.title = 'osef.me';
+  }, []);
+
   return (
     <Router>
       <div className="App">
