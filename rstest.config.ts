c0d5c7a907af5d6fc5cb967b1c4419a6fca81627
Glubus <osefcode@gmail.com>
2025-09-06 00:31:06 +0200

    chores: added junit

--- rstest.config.ts
+++ rstest.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from '@rstest/core';
 
 export default defineConfig({
   testEnvironment: 'jsdom', // or 'happy-dom'
+  reporters: [['junit', { outputPath: './junit.xml' }]],
 });
